e16d48d563933156dfdea2b04beb611bcf561e95
alvaromartinezu <alvaro.martinezu@opendeusto.es>
2024-07-01 11:54:55 +0200

    latencia arreglo

--- src/test/java/es/deusto/bilboHotels/controller/AutenticacionControllerPerformanceTest.java
+++ src/test/java/es/deusto/bilboHotels/controller/AutenticacionControllerPerformanceTest.java
@@ -48,8 +48,8 @@ public class AutenticacionControllerPerformanceTest {
 
     @Test
     @PerformanceTest
-    @JUnitPerfTest(threads = 2, durationMs = 5000)
-    @JUnitPerfTestRequirement(meanLatency = 500)
+    @JUnitPerfTest(threads = 2, durationMs = 1000)
+    @JUnitPerfTestRequirement(meanLatency = 100)
     public void testRegisterCustomerAccountSuccess() throws Exception {
         // Prepare mock data
         RegistroUsuarioDTO registroDTO = RegistroUsuarioDTO.builder()
@@ -71,8 +71,8 @@ public class AutenticacionControllerPerformanceTest {
 
     @Test
     @PerformanceTest
-    @JUnitPerfTest(threads = 2, durationMs = 5000)
-    @JUnitPerfTestRequirement(meanLatency = 500)
+    @JUnitPerfTest(threads = 2, durationMs = 1000)
+    @JUnitPerfTestRequirement(meanLatency = 100)
     public void testRegisterCustomerAccountFailureDueToExistingUsername() throws Exception {
         // Prepare mock data
         RegistroUsuarioDTO registroDTO = RegistroUsuarioDTO.builder()
@@ -97,8 +97,8 @@ public class AutenticacionControllerPerformanceTest {
 
     @Test
     @PerformanceTest
-    @JUnitPerfTest(threads = 2, durationMs = 5000)
-    @JUnitPerfTestRequirement(meanLatency = 500)
+    @JUnitPerfTest(threads = 2, durationMs = 1000)
+    @JUnitPerfTestRequirement(meanLatency = 100)
     public void testRegisterCustomerAccountFailureDueToValidationErrors() throws Exception {
         // Prepare mock data
         RegistroUsuarioDTO registroDTO = RegistroUsuarioDTO.builder()
